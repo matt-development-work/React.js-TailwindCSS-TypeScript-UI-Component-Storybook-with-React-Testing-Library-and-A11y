68859dd8ccd9c218cb4d6ad38ff1cad9b3d25394
Matt Eric <matt.dev.work@gmail.com>
2021-12-22 15:38:50 -0700

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+  ],
+  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  typescript: {
+    check: true, // type-check stories during Storybook build
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+    });
+
+    return config;
+  },
+};
