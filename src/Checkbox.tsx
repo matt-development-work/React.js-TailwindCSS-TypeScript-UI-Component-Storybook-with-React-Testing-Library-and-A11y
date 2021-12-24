b6e682a62e394c4c38f0c0be894b58c7efdcdb7f
Matt Eric <matt.dev.work@gmail.com>
2021-12-24 08:25:40 -0700

    Extract handleChange and add to input element

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -1,6 +1,7 @@
 import React, { forwardRef, Fragment, HTMLAttributes, useMemo } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faMinus } from '@fortawesome/free-solid-svg-icons';
+
 export interface Props extends HTMLAttributes<HTMLInputElement> {
   checked?: boolean;
   disabled?: boolean;
@@ -24,6 +25,10 @@ export const Checkbox = forwardRef<HTMLInputElement, Props>(
     },
     ref
   ) => {
+    const handleChange = (): void => {
+      !disabled && onChange();
+    };
+
     const hasValue: boolean = useMemo(
       () => checked || indeterminate,
       [checked, indeterminate]
@@ -35,14 +40,14 @@ export const Checkbox = forwardRef<HTMLInputElement, Props>(
           className={`absolute flex justify-center items-center h-4 w-4 m-1 rounded border border-green-500 ${
             !disabled && 'cursor-pointer'
           } ${hasValue && 'bg-green-500'} focus:outline-none focus-visible`}
-          onClick={() => !disabled && onChange()}
+          onClick={handleChange}
           tabIndex={0}
         >
           {hasValue && (
             <FontAwesomeIcon
               className="absolute text-white"
-              size="sm"
               icon={indeterminate ? faMinus : faCheck}
+              size="sm"
             />
           )}
         </span>
@@ -52,6 +57,7 @@ export const Checkbox = forwardRef<HTMLInputElement, Props>(
           className="hidden"
           disabled={disabled}
           id={id}
+          onChange={handleChange}
           ref={ref}
           tabIndex={-1}
           title={title}
