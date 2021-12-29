527ed09885e5c65d295e52cd3ec39c1f9ab2d87c
Matt Eric <matt.dev.work@gmail.com>
2021-12-28 23:09:13 -0700

    [Card] Add className prop

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,13 +2,17 @@ import React, { forwardRef, HTMLAttributes, ReactNode, useMemo } from 'react';
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
+  className: string;
   elevation?: string;
   hoverElevation?: string;
   square?: boolean;
 }
 
 export const Card = forwardRef<HTMLDivElement, Props>(
-  ({ children, elevation = 'md', hoverElevation, square, ...props }, ref) => {
+  (
+    { children, className, elevation = 'md', hoverElevation, square, ...props },
+    ref
+  ) => {
     const rounded: string | undefined = useMemo(
       () => (!square ? 'rounded' : undefined),
       [square]
@@ -16,7 +20,7 @@ export const Card = forwardRef<HTMLDivElement, Props>(
 
     return (
       <div
-        className={`absolute transition-shadow ease-in-out duration-500 p-2 shadow-${elevation} hover:shadow-${hoverElevation} ${
+        className={`${className} absolute transition-shadow ease-in-out duration-500 p-2 shadow-${elevation} hover:shadow-${hoverElevation} ${
           hoverElevation && 'cursor-pointer'
         } ${rounded}`}
         ref={ref}
