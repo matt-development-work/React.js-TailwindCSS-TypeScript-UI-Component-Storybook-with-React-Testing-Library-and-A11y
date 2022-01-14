3a2e0ba2eb03b6c308f40976ed29852cba9680a9
Matt Eric <matt.dev.work@gmail.com>
2022-01-14 09:30:33 -0700

    [Tree] Move class names to corresponding css file

--- src/components/Tree/Tree.tsx
+++ src/components/Tree/Tree.tsx
@@ -15,6 +15,7 @@ import React, {
 } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import './tree.css';
 
 interface ContextProps {
   data: TreeNode;
